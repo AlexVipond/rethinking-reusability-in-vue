bdbf17e7795f908506811c8f9b7c0022a45837b8
AlexVipond <alex.r.vipond@gmail.com>
2021-11-20 15:20:32 -0600

    truer representation of how Listbox would actually manage state

--- src/compound/Listbox.ts
+++ src/compound/Listbox.ts
@@ -1,6 +1,6 @@
 import {
   defineComponent, provide, inject,
-  ref, watch, computed, onMounted, onBeforeUpdate
+  shallowRef, ref, watch, computed
 } from 'vue'
 import type { PropType } from 'vue'
 
@@ -19,71 +19,72 @@ export const Listbox = defineComponent({
     },
   },
   setup (props, { slots, emit }) {
-    const optionsElements = ref<HTMLElement[]>([])
-    
-    const storeOptionsElement = (index: number, element: HTMLElement) => {
-      optionsElements.value[index] = element
+    const ids = shallowRef<{ [option: string]: string }>({})
+
+    for (const option of props.options) {
+      ids.value[option] = 'compound-listbox-option-' + totalIds++
     }
-
-    onBeforeUpdate(() => optionsElements.value = [])
-
-    const ids = ref([])
-    onMounted(() => ids.value = optionsElements.value
-      .map(() => 'compound-listbox-option-' + totalIds++))
     
-    const active = ref(0)
+    const active = ref(props.options[0])
     const ariaActivedescendant = computed(() => ids.value[active.value])
 
-    const activate = (index: number) => {
-      active.value = index
+    const activate = (option: string) => {
+      active.value = option
     }
 
-    const activatePrevious = (index: number) => {
+    const activatePrevious = (option: string) => {
+      const index = props.options.indexOf(option)
+
       if (index === 0) {
         return
       }
 
-      active.value = index - 1
+      active.value = props.options[index - 1]
     }
 
-    const activateNext = (index: number) => {
-      if (index === optionsElements.value.length - 1) {
+    const activateNext = (option: string) => {
+      const index = props.options.indexOf(option)
+
+      if (index === props.options.length - 1) {
         return
       }
 
-      active.value = index + 1
+      active.value = props.options[index + 1]
     }
 
-    const isActive = (index: number) => {
-      return index === active.value
+    const activateFirst = () => {
+      active.value = props.options[0]
     }
 
-    const selected = computed(() => props.options.indexOf(props.modelValue))
-
-    const select = (index: number) => {
-      emit('update:modelValue', props.options[index])
+    const activateLast = () => {
+      active.value = props.options[props.options.length - 1]
     }
 
-    const isSelected = (index: number) => {
-      return index === selected.value
+    const isActive = (option: string) => {
+      return option === active.value
     }
 
-    watch(
-      [active, selected],
-      () => {
-        optionsElements.value[active.value].focus()
-      },
-      { flush: 'post' }
-    )
+    const selected = computed(() => props.modelValue)
+
+    const select = (option: string) => {
+      emit('update:modelValue', option)
+    }
+
+    const isSelected = (option: string) => {
+      return option === selected.value
+    }
 
     provide(ListboxSymbol, {
       options: props.options,
-      storeOptionsElement,
       ids,
+      active,
       activate,
       activatePrevious,
       activateNext,
+      activateFirst,
+      activateLast,
       isActive,
+      selected,
       select,
       isSelected,
     })
@@ -99,6 +100,8 @@ export const Listbox = defineComponent({
       bindings: bindings.value,
       active,
       activate,
+      activateFirst,
+      activateLast,
       selected,
       select,
     })
@@ -114,60 +117,77 @@ export const ListboxOption = defineComponent({
   },
   setup (props, { slots }) {
     const {
-      options,
-      storeOptionsElement,
       ids,
+      active,
       activate,
       activatePrevious,
       activateNext,
+      activateFirst,
+      activateLast,
       isActive,
+      selected,
       select,
       isSelected,
     } = inject(ListboxSymbol)
 
-    const index = options.indexOf(props.option)
+    const getEl = shallowRef<() => HTMLElement>()
     
-    const id = computed(() => ids.value[index])
+    const id = ids.value[props.option]
 
-    return () => slots.default({
-      bindings: {
-        role: 'option',
-        id,
-        'aria-selected': isSelected(index),
-        tabindex: isSelected(index) ? 0 : -1,
-        ref: el => storeOptionsElement(index, el),
-        onMouseenter: () => activate(index),
-        onClick: () => select(index),
-        onKeydown: event => {
-          switch (event.key) {
-            case 'ArrowUp':
-              event.preventDefault()
-              if (event.metaKey) {
-                activate(0)
-                break
-              }
-              activatePrevious(index)
-              break
-            case 'ArrowDown':
-              event.preventDefault()
-              if (event.metaKey) {
-                activate(options.length - 1)
-                break
-              }
-              activateNext(index)
-              break
-            case 'Enter':
-            case ' ':
-              event.preventDefault()
-              select(index)
-              break
-          }
-        },
+    watch(
+      [active, selected],
+      () => {
+        if (isActive(props.option)) {
+          getEl.value().focus()
+        }
       },
-      isActive: () => isActive(index),
-      isSelected: () => isSelected(index),
-      activatePrevious: () => activatePrevious(index),
-      activateNext: () => activateNext(index),
-    })
+      { flush: 'post' }
+    )
+
+    return () => {
+      const rendered = slots.default({
+        bindings: {
+          role: 'option',
+          id,
+          'aria-selected': isSelected(props.option),
+          tabindex: isSelected(props.option) ? 0 : -1,
+          onMouseenter: () => activate(props.option),
+          onClick: () => select(props.option),
+          onKeydown: event => {
+            switch (event.key) {
+              case 'ArrowUp':
+                event.preventDefault()
+                if (event.metaKey) {
+                  activateFirst()
+                  break
+                }
+                activatePrevious(props.option)
+                break
+              case 'ArrowDown':
+                event.preventDefault()
+                if (event.metaKey) {
+                  activateLast()
+                  break
+                }
+                activateNext(props.option)
+                break
+              case 'Enter':
+              case ' ':
+                event.preventDefault()
+                select(props.option)
+                break
+            }
+          },
+        },
+        isActive: () => isActive(props.option),
+        isSelected: () => isSelected(props.option),
+        activatePrevious: () => activatePrevious(props.option),
+        activateNext: () => activateNext(props.option),
+      })
+
+      getEl.value = () => rendered[0].el as HTMLElement
+
+      return rendered
+    }
   }
 })
